c23137c97cef81568c83f90c153b2d80177a6840
DorianQA-WEB <qa.doriangrey@gmail.com>
2025-08-21 18:48:06 +0500

    Улучшение работы с API клиентами с помощью Enum для URI

--- clients/users/private_users_client.py
+++ clients/users/private_users_client.py
@@ -5,6 +5,8 @@ from clients.private_http_builder import get_private_http_client, Authentication
 from clients.users.user_schema import UpdateUserRequestSchema, GetUserResponseSchema
 import allure
 
+from tools.routes import APIRoutes
+
 
 class PrivateUsersClient(APIClient):
     """
@@ -18,7 +20,7 @@ class PrivateUsersClient(APIClient):
 
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.get("/api/v1/users/me")
+        return self.get(f"{APIRoutes.USERS}/me")
 
     @allure.step("Get user by id {user_id}")
     def get_user_api(self, user_id: str) -> Response:
@@ -28,7 +30,7 @@ class PrivateUsersClient(APIClient):
         :param user_id: Идентификатор пользователя.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.get(f"/api/v1/users/{user_id}")
+        return self.get(f"{APIRoutes.USERS}/{user_id}")
 
     @allure.step("Update user by id {user_id}")
     def update_user_api(self, user_id: str, request: UpdateUserRequestSchema) -> Response:
@@ -39,9 +41,9 @@ class PrivateUsersClient(APIClient):
         :param request: Словарь с email, lastName, firstName, middleName.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.patch(f"/api/v1/users/{user_id}", json=request.model_dump(by_alias=True))
+        return self.patch(f"{APIRoutes.USERS}/{user_id}", json=request.model_dump(by_alias=True))
 
-    @allure.step("Delete user by id {user_id}")
+    @allure.step("Delete  user by id {user_id}")
     def delete_user_api(self, user_id: str) -> Response:
         """
         Метод удаления пользователя по идентификатору.
@@ -49,7 +51,7 @@ class PrivateUsersClient(APIClient):
         :param user_id: Идентификатор пользователя.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.delete(f"/api/v1/users/{user_id}")
+        return self.delete(f"{APIRoutes.USERS}/{user_id}")
 
 
     def get_user(self, user_id: str) -> GetUserResponseSchema:
